8175e56f08359637264dd1343bb508c728f9b11c
Brian Burrous <brianon007@gmail.com>
2021-11-18 20:56:43 -0800

    Added route 'add' which adds data to firebase

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from app import app
+from firebase_setup import testRef
+
 
 @app.route('/')
 @app.route('/index')
@@ -7,3 +9,11 @@ def index():
     # return "<h1>Hello World</h1>"
     return render_template('main.html')
 
+@app.route('/add', methods=['POST'])
+def create():
+    try:
+        id = request.json['id']
+        testRef.document(id).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
--- firebase.py
+++
@@ -1,8 +0,0 @@
-import google.cloud
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("./key.json") #ENTER PATH LENGTH TO CREDENTIALS HERE
-app = firebase_admin.initialize_app(cred)
-
-store = firestore.Client('fair-myth-274206')
