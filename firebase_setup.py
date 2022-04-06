fec27df35ba1ea67684175407ae6b78bca2fbccc
Brian Burrous <brianon007@gmail.com>
2022-04-05 17:14:16 -0700

    Deleted some files

--- firebase_setup.py
+++
@@ -1,9 +0,0 @@
-import google.cloud
-import firebase_admin
-from firebase_admin import credentials, firestore, initialize_app
-
-cred = credentials.Certificate("key.json") #ENTER PATH LENGTH TO CREDENTIALS HERE
-app = initialize_app(cred)
-db = firestore.client()
-testRef = db.collection('test')
-
